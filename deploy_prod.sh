331dabf2b99ce8684788581712f75b84c4f7ab54
Daniel Milenkovic <daniel.milenkovic@notch-interactive.com>
2017-05-01 12:32:45 +0200

    try new config

--- deploy_prod.sh
+++ deploy_prod.sh
@@ -1,5 +1,2 @@
-# build project
-gradlew build
-
 # copy war file to production machine
 rsync -avz  -e "ssh -p 7454" build/libs/trvlr-0.0.1-SNAPSHOT.war centos@5.148.165.52:/usr/share/tomcat/webapps/trvlr-0.0.1-SNAPSHOT.war
