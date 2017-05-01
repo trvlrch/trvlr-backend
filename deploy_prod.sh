ad342761e0ca72712a49168afcda09ca61e5b26c
Daniel Milenkovic <daniel.milenkovic@notch-interactive.com>
2017-05-01 15:02:22 +0200

    use env var for serviceAccountJson path

--- deploy_prod.sh
+++ deploy_prod.sh
@@ -1,2 +1,2 @@
 # copy war file to production machine
-rsync -avz  -e "ssh -p 7454" build/libs/trvlr-0.0.1-SNAPSHOT.war centos@5.148.165.52:/usr/share/tomcat/webapps/trvlr-0.0.1-SNAPSHOT.war
+rsync -avz  -e "ssh -p 7454" build/libs/trvlr-0.0.1-SNAPSHOT.war centos@5.148.165.52:/var/www/trvlr_backend/java/trvlr_backend.war
